8f82752ced3377310df33a32b29a77928c8ec644
SaintEmbers <quantum108@gmail.com>
2015-07-11 17:28:59 -0700

    finished pseudoclassical

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,19 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.indx = 0
+  this.head = 0
+
 };
 
+ Queue.prototype.enqueue = function(value){
+   this[this.indx] = value
+   this.indx++
+ }
 
+Queue.prototype.dequeue = function(){
+  var val = this[this.head]
+  delete this[this.head]
+  this.head++
+  return val
+}
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.items = 0
+
 };
 
+Stack.prototype.push = function(value){
+  this[this.items] = value
+  this.items++
+}
 
+Stack.prototype.pop = function(){
+  if(this.items > 0)
+  this.items--
+  var value = this[this.items]
+  delete this[this.items]
+  return value
+
+}
+
+Stack.prototype.size = function(){
+  return this.items
+}
