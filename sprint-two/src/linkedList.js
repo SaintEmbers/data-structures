4ee5bb147be84007b0efb9c54d73d4e61a6981cf
SaintEmbers <quantum108@gmail.com>
2015-07-12 20:05:18 -0700

    finished linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = Node(value)
+    if(list.head === null){
+      list.head = node
+      list.tail = list.head
+    } else{
+      var currentTail = list.tail
+      currentTail.next = node
+    }
+      list.tail = node
   };
 
   list.removeHead = function(){
+    // debugger
+    var currentHead = list.head
+    console.log(list.head)
+    list.head = list.head.next
+    return currentHead.value
+
   };
 
   list.contains = function(target){
+    var isThere = false
+    function find(node){
+      debugger
+      if(node.value === target){
+         isThere = true
+      } else if(node.next){
+        find(node.next)
+      }
+    }
+    find(list.head)
+    return isThere
   };
 
   return list;
