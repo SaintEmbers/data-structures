a9c0409830b7d5903ea633f5168baa33c4e57ef7
SaintEmbers <quantum108@gmail.com>
2015-07-27 14:39:33 -0700

    finished hash table

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,20 @@
 var BinarySearchTree = function(value){
+  this.value = value;
+
+
 };
 
+BinarySearchTree.prototype.insert = function(){
 
+}
+
+BinarySearchTree.prototype.contains = function(){
+
+}
+
+BinarySearchTree.prototype.depthFirstLog = function(){
+
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,43 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var bucket = this._storage.get(i) || []
+  for(var j = 0; j < bucket.length; j++){
+    var pair = bucket[j]
+    if(pair[0] === k){
+      pair[1] = v
+      return
+    }
+  }
+  var duple = [k,v]
+  bucket.push(duple)
+  this._storage.set(i, bucket)
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var bucket = this._storage.get(i) || []
+  var val;
+  for(var j = 0; j < bucket.length; j++){
+  // debugger
+    var pair = bucket[j]
+    if(pair[0] === k){
+      val = pair[1]
+      return val
+    }
+  }
+  return null
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i) || []
+  for(var j = 0; j < bucket.length; j++){
+    var pair = bucket[j]
+    if(pair[0]===k){
+      bucket.splice(j,1)
+    }
+  }
 
 };
 
